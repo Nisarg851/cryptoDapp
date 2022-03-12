388b0c1e0364dab13af8c70c7dfc6dd8e38d080a
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-03-13 00:19:43 +0530

    Layout done

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,4 +1,4 @@
-export {default as Loader} from "./Loader";
+export {default as Loader} from "./CustomLoader";
 export {default as Navbar} from "./Navbar";
 export {default as Services} from "./Services";
 export {default as Transactions} from "./Transactions";
